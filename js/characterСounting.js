fdfe06fd4a711fa9295c99a49dc040cace468724
Milem <milemx11@gmail.com>
2024-02-10 23:54:36 +0500

    Fix regular expressions

--- js/characterСounting.js
+++ js/characterСounting.js
@@ -11,19 +11,19 @@ const copyIcon = document.getElementById('copyIcon');
 function updateCharacterCount(text) {
     totalChars.textContent = text.length;
     noSpaces.textContent = text.replace(/\s/g, '').length;
-};
+}
 
 function updateWordCount(text) {
-    const words = text.match(/\b\w+\b/g);
+    const words = text.match(/[\wа-яё'-]+/gi);
     wordCount.textContent = words ? words.length : 0;
-};
+}
 
 function updateCharacterTypeCount(text) {
-    cyrillicChars.textContent = text.match(/[а-я]/gi) ? text.match(/[а-я]/gi).length : 0;
+    cyrillicChars.textContent = text.match(/[а-яё]/gi) ? text.match(/[а-яё]/gi).length : 0;
     latinChars.textContent = text.match(/[a-z]/gi) ? text.match(/[a-z]/gi).length : 0;
     digitCount.textContent = text.match(/\d/g) ? text.match(/\d/g).length : 0;
-    specialChars.textContent = text.replace(/[a-zа-я\d\s]/gi, '').length;
-};
+    specialChars.textContent = text.replace(/[a-zа-яё\d\s]/gi, '').length;
+}
 
 function toggleCopyIconVisibility(text) {
     if (text.length > 0) {
@@ -34,7 +34,7 @@ function toggleCopyIconVisibility(text) {
     } else {
         copyIcon.style.display = 'none';
     }
-};
+}
 
 inputText.addEventListener('input', () => {
     const text = inputText.value;
